90c65bfc8b585c2733bc6099a741bf94dad4e830
Agimir <william@mesaindigo.com>
2024-08-13 23:18:07 -0500

    fuckin hooah

---
+++ data/data.lua
@@ -0,0 +1,16 @@
+
+-- base locations
+Data.Base = {
+    blip = {
+        loc = vec(0,0,0), spr = 1, clr = 1, scl = 0.6,
+        lbl = locale('blip_base')
+    },
+}
+
+-- carrier locations
+Data.Carrier = {
+    blip = {
+        loc = vec(0,0,0), spr = 1, clr = 1, scl = 0.6,
+        lbl = locale('blip_carrier')
+    },
+}
---
+++ fxmanifest.lua
@@ -0,0 +1,51 @@
+--[[    cfx.re information    ]]--
+use_experimental_fxv2_oal 'yes'
+fx_version 'cerulean'
+lua54 'yes'
+game 'gta5'
+
+--[[    resource information    ]]--
+name 'mi_military'
+author 'Agimir'
+version '0.0.0'
+repository 'https://github.com/Mesa-Indigo/mi_military'
+description 'ox_core script for vehicle interactions, shops, and garages'
+
+--[[    dependancies    ]]--
+dependencies { 
+    '/server:7290', '/onesync',
+    'ox_lib', 'ox_target', 'ox_core'
+}
+
+--[[    manifest information    ]]--
+shared_scripts {
+    -- overextended dependancies
+    '@ox_lib/init.lua',
+    '@ox_core/lib/init.lua',
+
+    -- core resources
+    'shared/*.lua',
+    'data/*.lua',
+    'utils/interface.lua'
+}
+
+client_scripts {
+    -- overextended dependancies
+    '@ox_core/imports/client.lua',
+
+    -- core resources
+    'client/**/*.lua',
+    'utils/client.lua'
+}
+
+server_scripts {
+    -- overextended dependancies
+    '@oxmysql/lib/MySQL.lua',
+    '@ox_core/imports/server.lua',
+
+    -- core resources
+    'server/**/*.lua',
+    'utils/server.lua'
+}
+
+files { 'locales/*.json' }
